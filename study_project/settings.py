8ac18a1b984f654ef5d4245bab44d4701f3d5bba
abdullaSafi <abdullashafi5@gmail.com>
2022-04-22 16:47:12 +0600

    ah motherfucking life! I swear I hate you bugs

--- study_project/settings.py
+++ study_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-@ex%g_ob996rp0%dxbbtja7f*0l8^jiv4f^i%j%qeej8=coomj
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['study-mate-app.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['study-mate-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
